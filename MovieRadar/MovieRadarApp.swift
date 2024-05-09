c9b79a0386839f80df38e919407cd2744b51b9c4
Sualp Danacı <sualpdnc@gmail.com>
2024-05-09 14:01:48 +0300

    Initial Commit

---
+++ MovieRadar/MovieRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRadarApp.swift
+//  MovieRadar
+//
+//  Created by Sualp Danacı on 9.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
